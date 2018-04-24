b6784439034eeef93ef4300857a2ce098d1aff29
camiloriosf <camilo.rios.f@gmail.com>
2018-04-24 15:16:41 -0300

    graphql + datastore

--- index.js
+++ index.js
@@ -1,29 +1,64 @@
 const express = require('express');
+var crypto = require('crypto');
 const bodyParser = require('body-parser');
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 const { makeExecutableSchema } = require('graphql-tools');
+const Datastore = require('@google-cloud/datastore');
 const PORT = 8080;
-// Some fake data
-const books = [
-  {
-    title: "Harry Potter and the Sorcerer's stone",
-    author: 'J.K. Rowling'
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton'
-  }
-];
+
+// Instantiate a datastore client
+const datastore = Datastore();
 
 // The GraphQL schema in string form
 const typeDefs = `
   type Query { books: [Book] }
-  type Book { title: String, author: String }
+  type Book { id: String, title: String, author: String }
+  type Mutation {
+    addBook (
+      title: String!
+      author: String!
+    ): [Book]
+  }
 `;
+
+function getBooks() {
+  const query = datastore.createQuery('books');
+
+  return datastore.runQuery(query).then(results => {
+    const entities = results[0];
+    return entities;
+  });
+}
+
+function insertBook(book) {
+  return datastore.save({
+    key: datastore.key('books'),
+    data: book
+  });
+}
 
 // The resolvers
 const resolvers = {
-  Query: { books: () => books }
+  Book: {
+    id: obj => {
+      return obj[datastore.KEY].id;
+    }
+  },
+  Query: {
+    books: () => {
+      const items = getBooks();
+      return items;
+    }
+  },
+  Mutation: {
+    addBook: (_, { title, author }) => {
+      return insertBook({ title, author })
+        .then(() => getBooks())
+        .then(books => {
+          return books;
+        });
+    }
+  }
 };
 
 // Put together a schema
@@ -34,6 +69,7 @@ const schema = makeExecutableSchema({
 
 // Initialize the app
 const app = express();
+app.enable('trust proxy');
 
 // The GraphQL endpoint
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
